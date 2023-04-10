374c711c0767e756dbec69173948399ef2d9033c
lyyubava <mac@MACs-MacBook-Pro.local>
2023-04-11 01:14:12 +0300

    Add go api

---
+++ models/setup.go
@@ -0,0 +1,24 @@
+package models
+
+import (
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDatabase() {
+	dsn := os.Getenv("DATABASE_CONNECTION_STRING")
+	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("Fail to connect to db")
+	}
+	err = database.AutoMigrate(&User{}, &Post{})
+
+	if err != nil {
+		return
+	}
+	DB = database
+}
